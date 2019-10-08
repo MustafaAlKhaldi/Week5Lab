508b3c21f461dc9a722bfce5e087a318ed766a3a
778766 <778766@elf8it221362.ACDM.DS.SAIT.CA>
2019-10-08 15:39:40 -0600

    Finished register.jsp Coding shippingList.jsp

---
+++ src/java/servlets/ShoppingListServlet.java
@@ -0,0 +1,50 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class ShoppingListServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+//        String action = request.getParameter("action");
+//        runServlet(request, response, action);
+        getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String action = request.getParameter("action");
+        runServlet(request, response, action);
+    }
+
+    protected void runServlet(HttpServletRequest request, HttpServletResponse response, String action)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        switch (action) {
+            case "register":
+                String name = request.getParameter("username");
+                session.setAttribute("username", name);
+                
+                getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+                break;
+            case "add":
+                getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+                break;
+            case "delete":
+                getServletContext().getRequestDispatcher("/WEB-INF/shoppingList.jsp").forward(request, response);
+                break;
+            case "logout":
+                session.invalidate();
+                getServletContext().getRequestDispatcher("/WEB-INF/register.jsp").forward(request, response);
+                break;
+        }
+    }
+}
